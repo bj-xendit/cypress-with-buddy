c4cc1b20e090b6264e8c2fe5702e94c9c5da0709
bj-xendit <bijay.pun@xendit.co>
2022-07-12 17:34:30 +0545

    add API test for create new order

--- cypress/e2e/api/orders.cy.js
+++ cypress/e2e/api/orders.cy.js
@@ -40,3 +40,60 @@ describe('Get product list',()=>{
     }))});
 
 
+
+describe('Create order',()=>{
+    it('create order - POST',()=>{
+
+        cy.request({
+            headers:{
+                'x-session-token': token
+            },
+            method:'POST',
+url:
+            
+          baseUrl  +  '/api/v2/order',
+           body:{
+            
+            
+                "cart": [
+                    {
+                                  "item_id": cartItem,
+                                  "item_variant_id": cartItemVariant,
+                                  "item_name": "Test name",
+                                  "item_variant_name": "Test variant",
+                                  "quantity": 1,
+                                  "price": 10000
+                              }
+                          ],
+                          "discount": 0,
+                          "total_cart_price": 10000,
+                          "total_price": 100,
+                          "currency": "IDR",
+                "shipment": {
+                  "sender": {
+                    "first_name": "john",
+                    "address": {
+                      "address": "testaddress"
+                    }
+                  },
+                  "recipient": {
+                    "mobile_phone": "+629000000000",
+                    "first_name": "Test One"
+                  },
+                  "method": "MERCHANT_ARRANGED",
+                  "fee": 0.0
+                },
+                "total_cart_price": 100000.0,
+                "payment": {
+                  "type": "PAYMENT_LINK"
+                }
+              }
+          
+        }
+
+        ).then((response)=>{
+          cy.wrap(response.body.id).should('not.be.empty');
+        })
+
+        })
+    })
